14faa7bd3c6a3d85d52e582bf63a9cb8fd7db4cf
Kauan Souza <k1one.g.vitor@gmail.com>
2023-01-30 15:52:52 -0300

    Home styling

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,13 +1,13 @@
-import Navbar from './components/Navbar';
-import './assets/styles/custom.scss';
+import Home from 'pages/Home';
+
+import 'assets/styles/custom.scss';
 import './App.css';
 
 
 const App = () => {
   return (
     <>
-      <Navbar/>
-      <h1>Hello DSCatalog!</h1>
+      <Home />
     </>
   );
 }
